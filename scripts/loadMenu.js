68ee656de93eeddf497a40d29934033f0f00a3a5
Ingvar <jamessoer91@gmail.com>
2024-09-14 13:14:28 +0300

    another fix

--- scripts/loadMenu.js
+++ scripts/loadMenu.js
@@ -1,9 +1,9 @@
 //загрузка nav меню на страницы
-document.addEventListener('DOMContentLoaded', function () {
-  fetch('partials/menu.html')
+export function loadMenu() {
+  fetch('/partials/menu.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('menu').innerHTML = data;
     })
     .catch(error => console.log('Ошибка загрузки меню:', error));
-});
+}
