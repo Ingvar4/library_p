684eeab4c2fb6f665a552fa484f298960b9cb3f3
Ingvar <jamessoer91@gmail.com>
2024-09-18 22:10:56 +0300

    css

--- scripts/script2(sandbox).js
+++ scripts/script2(sandbox).js
@@ -2,5 +2,10 @@ document.getElementById("demo").innerHTML = 2 + 2;
 
 let info = "Hello, world!";
 
+function calculateArea(width, height) {
+  let area = width * height;
+  return area;
+}
 
-document.getElementById("demo2").innerHTML = info;
+let result = calculateArea(3, 4)
+document.getElementById("demo2").innerHTML = result;
