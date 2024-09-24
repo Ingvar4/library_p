b2904bf78ef225fa10c6a828380def926f941141
Ingvar <jamessoer91@gmail.com>
2024-09-24 17:58:07 +0300

    js ready

--- scripts/ready-ex.js
+++ scripts/ready-ex.js
@@ -98,3 +98,23 @@ function pickComputerMove() {
   }
   return computerMove;
 }
+
+//Amazon Shipping Calculator
+function handleCostKeydown(event) {
+  if (event.key === 'Enter') {
+    calcilateTotal();
+  }
+}
+
+function calcilateTotal() {
+  const inputElement = document.querySelector('.js-cost-input');
+  let cost = Number(inputElement.value);
+
+  if (cost < 40) {
+    cost = cost + 10;
+  }
+
+  document.querySelector('.js-total-cost')
+    .innerHTML = `$${cost}`
+}
+
